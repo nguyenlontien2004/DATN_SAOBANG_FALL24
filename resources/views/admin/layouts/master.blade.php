ca109a859f662824ea2f5077a7ffdc5810a8b8db
nguyentienloc <locnguyen13504kk8386@gmail.com>
2024-10-29 11:52:45 +0700

    Nguyễn Tiến Lộc PH35747 Sửa code developer

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -3,15 +3,9 @@
 @include('admin.layouts.partials.head') <!-- Include phần head -->
 
 <body>
-    <div class="wrapper">
-        @include('admin.layouts.partials.sidebar')
-        <div class="main-panel">
-
-            @yield('noidung')
-            @include('admin.layouts.partials.footer')
-                @yield('jsCreateSeat')
-        </div>
-    </div>
+    @yield('noidung')
+    @include('admin.layouts.partials.footer')
+    @yield('jsCreateSeat')
 </body>
 
 </html>
