0f9c88d5b7702f59b023e023f1df9df12f2451df
ngohoangphuc <phucnhph34092@fpt.edu.vn>
2024-11-29 00:42:57 +0700

    gui merge

--- database/migrations/2024_10_15_070433_create_ves_table.php
+++ database/migrations/2024_10_15_070433_create_ves_table.php
@@ -29,6 +29,7 @@ return new class extends Migration
             $table->string('phuong_thuc_thanh_toan')->nullable();
             $table->boolean('trang_thai')->default(true);
             $table->timestamps();
+            
         });
     }
 
